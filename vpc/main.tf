63431c20600c78b3ecb81d32238abc76ab431cae
agray998 <a.gray.4797@gmail.com>
2021-10-20 11:23:36 +0100

    specified map public ip for subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,7 +9,8 @@ resource "aws_vpc" "example-vpc" {
 resource "aws_subnet" "example-sub" {
   vpc_id     = aws_vpc.example-vpc.id
   cidr_block = "10.0.1.0/24"
-
+  map_public_ip_on_launch = true
+  
   tags = {
     Name = "demo"
   }
